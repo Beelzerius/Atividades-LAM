a9cbed96c56ac4edf3679b2449e56cd983ed63a4
legendofeuz@gmail.com <legendofeuz@gmail.com>
2018-03-01 10:11:51 -0300

    Adicionado ExemploActivity

---
+++ Exemplos/AndroidStudio/1-Bim/ExemploActivity/app/src/main/java/com/example/alunos/exemploactivity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.alunos.exemploactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void MudaTexto(View v) {
+        TextView etiqueta = (TextView) findViewById(R.id.textView);
+        etiqueta.setText(getResources().getString(R.string.lblHello2));
+    }
+}
