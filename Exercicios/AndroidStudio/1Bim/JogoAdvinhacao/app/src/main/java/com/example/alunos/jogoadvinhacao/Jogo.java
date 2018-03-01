050441d10fc8e8955c487138f2a5d24dd55546c2
legendofeuz@gmail.com <legendofeuz@gmail.com>
2018-03-01 10:35:56 -0300

    Organizado o repositorio e adcionado o exercicio do Jogo da Adivinhação

---
+++ Exercicios/AndroidStudio/1Bim/JogoAdvinhacao/app/src/main/java/com/example/alunos/jogoadvinhacao/Jogo.java
@@ -0,0 +1,16 @@
+package com.example.alunos.jogoadvinhacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Jogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+
+}
