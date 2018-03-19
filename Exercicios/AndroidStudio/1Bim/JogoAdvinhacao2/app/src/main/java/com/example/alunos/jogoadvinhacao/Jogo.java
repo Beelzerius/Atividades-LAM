955f85543de029fef6c508199640616f518ebf85
legendofeuz@gmail.com <legendofeuz@gmail.com>
2018-03-19 12:26:06 -0300

    Terminado o jogo adv 2

--- Exercicios/AndroidStudio/1Bim/JogoAdvinhacao2/app/src/main/java/com/example/alunos/jogoadvinhacao/Jogo.java
+++ Exercicios/AndroidStudio/1Bim/JogoAdvinhacao2/app/src/main/java/com/example/alunos/jogoadvinhacao/Jogo.java
@@ -1,16 +1,20 @@
 package com.example.alunos.jogoadvinhacao;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import java.util.Random;
 import android.widget.*;
+import android.*;
 
 public class Jogo extends AppCompatActivity {
     public int numero;
     public int tentativas = 0;
-    public int limite = 1000;
+    public int limite = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,8 @@ public class Jogo extends AppCompatActivity {
         return (gerador.nextInt()%limite)+1;
     }
     public void chutarNumero(View v) {
+        SharedPreferences arquivo = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = arquivo.edit();
         int chute = 0;
         TextView etiqueta = (TextView) findViewById(R.id.rtl_mensagem);
         EditText userInput = findViewById(R.id.pt_numero);
@@ -33,6 +39,15 @@ public class Jogo extends AppCompatActivity {
                 button.setText(R.string.lblRestart);
                 userInput.setText("");
                 etiqueta.setText(R.string.lblWin);
+                String rec = arquivo.getString("Best","0");
+                int melhor = Integer.parseInt(rec);
+                if (rec == "0" || melhor>tentativas) {
+                    editor.putString("Best",Integer.toString(tentativas));
+                }
+                rec = arquivo.getString("Hist","");
+                rec = Integer.toString(tentativas)+"/n"+rec;
+                editor.putString("Best",rec);
+
             }else{
                 if(numero>chute){
                     userInput.setText("");
@@ -51,6 +66,10 @@ public class Jogo extends AppCompatActivity {
         }
 
 
+    }
+    public void irPlacar(View v){
+        Intent i = new Intent(Jogo.this, Placar.class);
+        startActivity(i);
     }
 
 }
--- Exercicios/AndroidStudio/1Bim/JogoAdvinhacao2/app/src/main/java/com/example/alunos/jogoadvinhacao/Placar.java
+++ Exercicios/AndroidStudio/1Bim/JogoAdvinhacao2/app/src/main/java/com/example/alunos/jogoadvinhacao/Placar.java
@@ -1,5 +1,6 @@
 package com.example.alunos.jogoadvinhacao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
@@ -7,6 +8,7 @@ import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Placar extends AppCompatActivity {
@@ -15,12 +17,20 @@ public class Placar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placar);
-        carregar();
-    }
-    public void carregar(){
+
+ //   }
+//    public void carregar(View v){
         SharedPreferences arquivo = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = arquivo.edit();
         String melhor = arquivo.getString("Best","");
         String historico = arquivo.getString("Hist","");
-
+        TextView best = (TextView) findViewById(R.id.rtl_best);
+        TextView hist = (TextView) findViewById(R.id.rtl_hist);
+        best.setText(melhor);
+        hist.setText(historico);
+    }
+    public void irJogo(View v){
+        Intent i = new Intent(Placar.this, Jogo.class);
+        startActivity(i);
     }
 }
